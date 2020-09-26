e68af6ded64c3f28e28d25aab683d7ccd5270a26
贺飞 <ff@163.com>
2020-09-26 16:42:17 +0800

    我是李思  我的建议代码您看看

--- src/main/java/com/atguigu/bean/love/love1.java
+++ src/main/java/com/atguigu/bean/love/love1.java
@@ -6,6 +6,9 @@ public class love1 {
         System.out.println("2020年9月26日16:11:22");
         System.out.println("今天是练习的日子啊对不:2020年9月26日16:21:58");
         System.out.println("今天是练习的日子啊对:啦啦啦啦啦啦");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("我是李四");
+        }
     }
 
 
