b01a4bdcb4fe678514df8d8ee7e593edbc128779
贺飞 <ff@163.com>
2020-09-26 16:15:32 +0800

    第一次时间

---
+++ src/main/java/com/atguigu/bean/love/love1.java
@@ -0,0 +1,10 @@
+package com.atguigu.bean.love;
+
+public class love1 {
+    public static void main(String[] args) {
+        System.out.println("-------------");
+        System.out.println("2020年9月26日16:11:22");
+    }
+
+
+}
